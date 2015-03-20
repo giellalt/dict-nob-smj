d9897284048c6c67a78395a64534194618363343
Kevin Unhammer <unhammer@fsfe.org>
2015-03-20 07:48:56 +0000

    macify

--- inc/some-stats.sh
+++ inc/some-stats.sh
@@ -5,11 +5,11 @@ pct_bad () {
         grep -q ^@ "$f" || continue
         bad=$(grep -c ^@ "$f")
         tot=$(sed 's/^[@+?-]//' "$f" |sort -u|wc -l)
-        diff=$(echo | awk "{printf \"%.2f %\", 100*${bad}/${tot}}")
+        diff=$(echo | gawk "{printf \"%.2f %\", 100*${bad}/${tot}}")
         echo  "$f	${bad}	${tot}	${diff}"
     done |
         sort -nrt$'\t' -k4,4 |
-        awk 'BEGIN{OFS=FS="\t"} {b+=$2;t+=$3;print} END{printf " \nSUM\t%d\t%d\t%.2f %", b, t, 100*b/t}'
+        gawk 'BEGIN{OFS=FS="\t"} {b+=$2;t+=$3;print} END{printf "·\nSUM\t%d\t%d\t%.2f %\n", b, t, 100*b/t}'
 }
 cat <(echo -e "FILENAME\tMARKED BAD\tTOTAL PAIRS\tPCT BAD") <(pct_bad) |
-    column -ts$'\t'
+    column -ts$'\t' -c90
